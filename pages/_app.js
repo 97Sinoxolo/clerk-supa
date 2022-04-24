2c626649c3d13754dc092b9499f3b70b397257ad
Sinoxolo Sooi <snoxy.sooi@gmail.com>
2022-04-24 22:02:10 +0200

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import '../styles/globals.css'
+import { ClerkProvider } from "@clerk/nextjs";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function ClerkSupabaseApp({ Component, pageProps }) {
+  return (
+    <ClerkProvider>
+      <Component {...pageProps}/>
+    </ClerkProvider>
+  );
 }
 
-export default MyApp
+export default ClerkSupabaseApp;
